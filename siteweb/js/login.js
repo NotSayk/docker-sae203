c9537a2a1a93dca7626d0001666e5128f7a6d4c1
NotSayk <samuampe@gmail.com>
2025-04-29 12:56:12 +0200

    Ajout boutton partager et télécharger

--- siteweb/js/login.js
+++ siteweb/js/login.js
@@ -4,9 +4,14 @@ document.getElementById('login-form').addEventListener('submit', function (e)
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value;
 
+    if (username.endsWith('.mp4') || username.includes('profilePhotoUrl') ||  username.includes('commentaires')) {
+        alert("Le nom d'utilisateur ne peut pas contenir 'profilePhotoUrl' ni se terminer par .mp4.");
+        return;
+    }
+
     const savedPassword = localStorage.getItem(username);
     if (savedPassword && savedPassword === password) 
-        {
+    {
         sessionStorage.setItem("user", username);
         window.location.href = "index.html";
     } 
